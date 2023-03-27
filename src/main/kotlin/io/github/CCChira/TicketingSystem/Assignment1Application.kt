920f8778617e289ba30bc8cebb47977a49d1bef2
CristiChira <cristian.chira@evozon.com>
2023-03-27 18:12:38 +0300

    initial commit

---
+++ src/main/kotlin/io/github/CCChira/TicketingSystem/Assignment1Application.kt
@@ -0,0 +1,11 @@
+package io.github.CCChira.TicketingSystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Assignment1Application
+
+fun main(args: Array<String>) {
+	runApplication<Assignment1Application>(*args)
+}
---
+++ src/test/kotlin/io/github/CCChira/TicketingSystem/Assignment1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.CCChira.TicketingSystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
